03efd1ac4a757d81f860eb0cda86f00c6297fd73
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-12-19 15:20:15 +0100

    add axios and delete hello component

---
+++ src/main/resources/front-end/src/components/Pizza.vue
@@ -0,0 +1,35 @@
+<template>
+  
+</template>
+
+<script>
+import axios from "axios";
+import { response } from "express";
+
+const URL_PIZZE = "http://localhost:8080/api/1/pizze"
+
+export default {
+    name: 'Pizza',
+
+    data(){
+        return{
+            
+        };
+    },
+
+    methods: {
+
+    },
+
+    created(){
+        axios.get( URL_PIZZE + '/all')
+        .then((response) => {
+            console.log(response);
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
